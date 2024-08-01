a80ed3badc3788216c6f69a58f78837f3752bf2a
jodinotnice <jordan.akpovi@gmail.com>
2024-08-01 20:02:40 +0200

    Création des functions add, substract, multiply, divide et operate

---
+++ index.js
@@ -0,0 +1,51 @@
+function add(a, b) {
+  const numberA = a;
+  const operator = "+";
+  const numberB = b;
+
+  return numberA + operator + numberB;
+}
+
+function substract(a, b) {
+  const numberA = a;
+  const operator = "-";
+  const numberB = b;
+
+  return numberA + operator + numberB;
+}
+
+function multiply(a, b) {
+  const numberA = a;
+  const operator = "*";
+  const numberB = b;
+
+  return numberA + operator + numberB;
+}
+
+function divide(a, b) {
+  const numberA = a;
+  const operator = "/";
+  const numberB = b;
+
+  return numberA + operator + numberB;
+}
+
+function operate(a, operator, b) {
+  if (operator === "+") {
+    return add(a, b);
+  } else if (operator === "-") {
+    return substract(a, b);
+  } else if (operator === "*") {
+    return multiply(a, b);
+  } else if (operator === "/") {
+    return divide(a, b);
+  }
+}
+
+console.log(operate(1, "+", 3));
+
+console.log(operate(1, "-", 3));
+
+console.log(operate(1, "*", 3));
+
+console.log(operate(1, "/", 3));
